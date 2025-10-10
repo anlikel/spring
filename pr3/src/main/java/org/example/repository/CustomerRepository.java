19f76e2a48a3272c9554a4377eb68259a1f797d6
anlikel <antongubenko2@gmail.com>
2025-10-10 15:32:42 +0300

    test crud repository

--- pr3/src/main/java/org/example/repository/CustomerRepository.java
+++ pr3/src/main/java/org/example/repository/CustomerRepository.java
@@ -2,9 +2,10 @@ package org.example.repository;
 
 import org.example.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
+public interface CustomerRepository extends CrudRepository<Customer,Long> {
     List<Customer> findByLastName(String lastName);
 }
